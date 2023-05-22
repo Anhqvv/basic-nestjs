4ef125cb74d44ba47231575cb8cde8f10a2f503c
Anhqvv <anhqvv2022@gmail.com>
2023-05-22 15:33:50 +0700

    # ENV configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { MongooseModule } from '@nestjs/mongoose'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 @Module({
-  // imports: [UsersModule],
+  imports: [
+    // MongooseModule.forRoot(
+    //   'mongodb+srv://qamultiboot:d0zVDLPqeyrzpiz9@cluster0.bkhuapy.mongodb.net/hoidanit',
+    // ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
