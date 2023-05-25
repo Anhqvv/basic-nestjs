b035e4293ba2b612bdcced61f288c1980d9ae0cc
Anhqvv <anhqvv2022@gmail.com>
2023-05-25 14:36:07 +0700

    # Validation Pipe

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,21 @@
+import { IsEmail, IsNotEmpty } from 'class-validator'
+
 //data transfer object
 export class CreateUserDto {
+  @IsEmail(
+    {},
+    {
+      message: 'Email khong dung dinh dang',
+    },
+  )
+  @IsNotEmpty({
+    message: 'Email khong duoc de trong',
+  })
   email: string
+
+  @IsNotEmpty()
   password: string
+
   username: string
   address: string
 }
