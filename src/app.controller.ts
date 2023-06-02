44779d5a2c5a8a93566c5f2b4c5947fce38fa9ff
Anhqvv <anhqvv2022@gmail.com>
2023-06-02 20:41:01 +0700

    #  Enable authentication globally

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -11,6 +11,7 @@ import { ConfigService } from '@nestjs/config'
 import { AuthGuard } from '@nestjs/passport'
 import { LocalAuthGuard } from './auth/local-auth.guard'
 import { AuthService } from './auth/auth.service'
+import { JwtAuthGuard } from './auth/jwt-auth.guard'
 
 @Controller()
 export class AppController {
@@ -20,9 +21,15 @@ export class AppController {
     private authService: AuthService,
   ) {}
 
-  @UseGuards(LocalAuthGuard)
+  // @UseGuards(LocalAuthGuard)
   @Post('login')
   handleLogin(@Request() req) {
     return this.authService.login(req.user)
+  }
+
+  // @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
